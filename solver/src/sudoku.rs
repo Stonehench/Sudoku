20ca96b0dc24b309b5c00fedf5792adb72237a24
jegkanikkeheddemike <54175974+jegkanikkeheddemike@users.noreply.github.com>
2024-02-21 14:26:01 +0100

    scaffolding

--- solver/src/sudoku.rs
+++ solver/src/sudoku.rs
@@ -1,8 +1,10 @@
+
+
 pub struct Sudoku {
     sizex: usize,
     sizey: usize,
     cells: Vec<Cell>,
-    rules: Vec<RuleInstance>
+    rules: Vec<RuleInstance>,
 }
 
 pub struct Cell {
@@ -11,6 +13,7 @@ pub struct Cell {
 
 pub trait Rule {
     fn updates(&self, sudoku: &Sudoku, index: usize) -> Vec<usize>;
+    fn is_legal(&self, sudoku: &Sudoku, index: usize, value: u16) -> bool;
 }
 
 pub struct RuleInstance(Box<dyn Rule>);
