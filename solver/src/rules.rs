88bcb36d899605370197f696d206419448c8b49c
jegkanikkeheddemike <54175974+jegkanikkeheddemike@users.noreply.github.com>
2024-02-21 14:59:34 +0100

    row / column rules

--- solver/src/rules.rs
+++ solver/src/rules.rs
@@ -5,14 +5,31 @@ pub trait Rule {
     fn is_legal(&self, sudoku: &Sudoku, index: usize, value: u16) -> bool;
 }
 
-pub struct RowColumnRule;
+pub struct RowRule;
 
-impl Rule for RowColumnRule {
+impl Rule for RowRule {
     fn updates(&self, sudoku: &Sudoku, index: usize) -> Vec<usize> {
+        let column = index / sudoku.size;
+        (0..sudoku.size)
+            .map(|i| i + column)
+            .filter(|i| *i != index)
+            .collect()
+    }
 
-        //let rowindex = index % sudoku.
-
+    fn is_legal(&self, sudoku: &Sudoku, index: usize, value: u16) -> bool {
         todo!()
+    }
+}
+
+pub struct ColumnRule;
+
+impl Rule for ColumnRule {
+    fn updates(&self, sudoku: &Sudoku, index: usize) -> Vec<usize> {
+        let row = index % sudoku.size;
+        (0..sudoku.size)
+            .map(|i| i + row)
+            .filter(|i| *i != index)
+            .collect()
     }
 
     fn is_legal(&self, sudoku: &Sudoku, index: usize, value: u16) -> bool {
