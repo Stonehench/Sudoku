dc800f575b95d1e27a2a80541765684c59de306f
jegkanikkeheddemike <54175974+jegkanikkeheddemike@users.noreply.github.com>
2024-02-21 15:58:20 +0100

    kan læse filer

--- solver/src/sudoku.rs
+++ solver/src/sudoku.rs
@@ -1,7 +1,8 @@
-use std::{num::ParseIntError, ops::Range, str::FromStr};
+use std::{fs, num::ParseIntError, ops::Range, process::Command, str::FromStr};
 
 use crate::rules::{ColumnRule, RowRule, Rule};
 
+#[derive(Debug)]
 pub struct Sudoku {
     pub size: usize,
     pub cells: Vec<Cell>,
@@ -13,12 +14,19 @@ impl Sudoku {
         Self {
             size,
             cells: (0..size * size)
-                .map(|_| Cell::new_with_range(0..(size as u16)))
+                .map(|_| Cell::new_with_range(1..(size as u16 + 1)))
                 .collect(),
             rules,
         }
     }
-    fn set_cell(&mut self, n: u16, index: usize) {}
+    fn set_cell(&mut self, n: u16, index: usize) {
+        self.cells[index] = Cell::single(n);
+        for rule in &self.rules {
+            for inner_index in rule.updates(&self, index) {
+                self.cells[inner_index].remove(n);
+            }
+        }
+    }
 }
 
 impl FromStr for Sudoku {
@@ -27,7 +35,9 @@ impl FromStr for Sudoku {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut sudoku = Sudoku::new(9, vec![Box::new(RowRule), Box::new(ColumnRule)]);
 
-        for (index, part) in s.split(',').enumerate() {
+        let (uløst, _løsning) = s.split_once("\n\n").unwrap();
+
+        for (index, part) in uløst.split(',').map(str::trim).enumerate() {
             let n = part.parse()?;
             if n != 0 {
                 sudoku.set_cell(n, index);
@@ -37,15 +47,29 @@ impl FromStr for Sudoku {
         Ok(sudoku)
     }
 }
-
+#[derive(Debug)]
 pub struct Cell {
     pub available: Vec<u16>,
 }
 
 impl Cell {
+    fn single(n: u16) -> Self {
+        Self { available: vec![n] }
+    }
     fn new_with_range(range: Range<u16>) -> Self {
         Self {
             available: range.collect(),
         }
     }
+    fn remove(&mut self, n: u16) {
+        self.available.retain(|i| *i != n);
+    }
+}
+
+#[test]
+fn read_file_test() {
+    let file_str = fs::read_to_string("./sudoku2").unwrap();
+    let sudoku: Sudoku = file_str.parse().unwrap();
+
+    println!("{sudoku:#?}");
 }
