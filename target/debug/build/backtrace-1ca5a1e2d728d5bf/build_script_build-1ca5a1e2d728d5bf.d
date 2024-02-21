035c42b96f996000a228f100f01f764395f3909b
jegkanikkeheddemike <54175974+jegkanikkeheddemike@users.noreply.github.com>
2024-02-21 13:40:30 +0100

    gitignore

--- target/debug/build/backtrace-1ca5a1e2d728d5bf/build_script_build-1ca5a1e2d728d5bf.d
+++
@@ -1,5 +0,0 @@
-/home/thor/Insync/thor.f.skipper@gmail.com/Google_Drive/dev/skole/sudoku/target/debug/build/backtrace-1ca5a1e2d728d5bf/build_script_build-1ca5a1e2d728d5bf: /home/thor/.cargo/registry/src/index.crates.io-6f17d22bba15001f/backtrace-0.3.69/build.rs
-
-/home/thor/Insync/thor.f.skipper@gmail.com/Google_Drive/dev/skole/sudoku/target/debug/build/backtrace-1ca5a1e2d728d5bf/build_script_build-1ca5a1e2d728d5bf.d: /home/thor/.cargo/registry/src/index.crates.io-6f17d22bba15001f/backtrace-0.3.69/build.rs
-
-/home/thor/.cargo/registry/src/index.crates.io-6f17d22bba15001f/backtrace-0.3.69/build.rs:
