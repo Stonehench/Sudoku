7b34b007c9b95bfe2b4525ae62df3a154770e7e3
jegkanikkeheddemike <54175974+jegkanikkeheddemike@users.noreply.github.com>
2024-02-21 14:40:18 +0100

    definitionps

--- solver/src/rules.rs
+++ solver/src/rules.rs
@@ -4,3 +4,18 @@ pub trait Rule {
     fn updates(&self, sudoku: &Sudoku, index: usize) -> Vec<usize>;
     fn is_legal(&self, sudoku: &Sudoku, index: usize, value: u16) -> bool;
 }
+
+pub struct RowColumnRule;
+
+impl Rule for RowColumnRule {
+    fn updates(&self, sudoku: &Sudoku, index: usize) -> Vec<usize> {
+
+        //let rowindex = index % sudoku.
+
+        todo!()
+    }
+
+    fn is_legal(&self, sudoku: &Sudoku, index: usize, value: u16) -> bool {
+        todo!()
+    }
+}
--- solver/src/sudoku.rs
+++ solver/src/sudoku.rs
@@ -1,13 +1,41 @@
+use std::ops::Range;
+
 use crate::rules::Rule;
 
 pub struct Sudoku {
-    sizex: usize,
-    sizey: usize,
-    cells: Vec<Cell>,
-    rules: Vec<Box<dyn Rule>>,
+    pub sizex: usize,
+    pub sizey: usize,
+    pub cells: Vec<Cell>,
+    pub rules: Vec<Box<dyn Rule>>,
+}
+
+impl Sudoku {
+    pub fn new(sizex: usize, sizey: usize, rules: Vec<Box<dyn Rule>>) -> Self {
+        let range = if sizex == sizey {
+            0..(sizex as u16)
+        } else {
+            todo!("Hvad skal der være i cellerne når jeg ikke kan mat?")
+        };
+
+        Sudoku {
+            sizex,
+            sizey,
+            cells: (0..sizex * sizey)
+                .map(|_| Cell::new_with_range(range.clone()))
+                .collect(),
+            rules,
+        }
+    }
 }
 
 pub struct Cell {
     available: Vec<u16>,
 }
 
+impl Cell {
+    fn new_with_range(range: Range<u16>) -> Self {
+        Self {
+            available: range.collect(),
+        }
+    }
+}
