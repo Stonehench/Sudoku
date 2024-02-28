f97887e2e448d32eb025386c4ceaff9c8f14dd83
Stonehench <121813975+Stonehench@users.noreply.github.com>
2024-02-28 13:09:01 +0100

    lille ui update

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:sudoku/src/rust/api/simple.dart';
+import 'package:flutter/services.dart';
 import 'package:sudoku/src/rust/frb_generated.dart';
 
 Future<void> main() async {
   await RustLib.init();
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final myController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -22,15 +26,23 @@ class MyApp extends StatelessWidget {
               SizedBox(
                 width: 250,
                 child: TextField(
-                  
+                  controller: myController,
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly],
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: 'Sudoku size',
                   ),
                 ),
               ),
+              myController.text.isNotEmpty ? Text('This results in ' + pow(int.parse(myController.text),3).toString()) : Text(''),
+              // Skal laves til en stateful widget så vidt jeg husker før man kan opdatere UI'en
               TextButton(
-                onPressed: () => print('du har klikket på knappen'),
+                onPressed: () => {
+                  print('du har klikket på knappen med værdien ' + myController.text),
+                  myController.clear()
+                  },
                 child: Text(
                   'Create Sudoku'
                 ),
