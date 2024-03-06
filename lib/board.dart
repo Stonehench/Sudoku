22a364a0ad70a7eeb021adf3d3a200fb464430c9
Stonehench <121813975+Stonehench@users.noreply.github.com>
2024-03-06 15:11:42 +0100

    Zoom funktion på brættet

--- lib/board.dart
+++ lib/board.dart
@@ -20,41 +20,45 @@ class _BoardState extends State<Board> {
       child: SizedBox(
         height: 340,
         width: 340,
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Container(color: const Color.fromARGB(255, 19, 22, 54)),
-            GridView.builder(
-              padding: EdgeInsets.zero,
-              itemCount: state.board.length,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: sqrt(state.size).toInt(),
-                  crossAxisSpacing: 2,
-                  mainAxisSpacing: 2),
-              itemBuilder: (context, index) {
-                return Container(
-                  color: const Color.fromARGB(255, 127, 132, 177),
-                );
-              },
-            ),
-            ListenableBuilder(
-              listenable: state,
-              builder: (ctx, _) => GridView.builder(
-                physics: const NeverScrollableScrollPhysics(),
+        child: InteractiveViewer(
+          minScale: 1,
+          maxScale: 5.0,
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(color: const Color.fromARGB(255, 19, 22, 54)),
+              GridView.builder(
                 padding: EdgeInsets.zero,
                 itemCount: state.board.length,
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: state.size,
-                  crossAxisSpacing: 2,
-                  mainAxisSpacing: 2,
-                ),
-                itemBuilder: (ctx, index) {
-                  return Cell(state.board[index], index, state.board.length,
-                      state.initialClues.contains(index));
+                    crossAxisCount: sqrt(state.size).toInt(),
+                    crossAxisSpacing: 2,
+                    mainAxisSpacing: 2),
+                itemBuilder: (context, index) {
+                  return Container(
+                    color: const Color.fromARGB(255, 127, 132, 177),
+                  );
                 },
               ),
-            )
-          ],
+              ListenableBuilder(
+                listenable: state,
+                builder: (ctx, _) => GridView.builder(
+                  physics: const NeverScrollableScrollPhysics(),
+                  padding: EdgeInsets.zero,
+                  itemCount: state.board.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: state.size,
+                    crossAxisSpacing: 2,
+                    mainAxisSpacing: 2,
+                  ),
+                  itemBuilder: (ctx, index) {
+                    return Cell(state.board[index], index, state.board.length,
+                        state.initialClues.contains(index));
+                  },
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
