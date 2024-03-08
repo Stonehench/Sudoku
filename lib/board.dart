000e556ea20a9ed88f1a60d86c6ae2925868afaa
Pinkymus <katinkaspangtoft@gmail.com>
2024-03-08 16:10:42 +0100

    X-Rule Gui Works

--- lib/board.dart
+++ lib/board.dart
@@ -15,6 +15,11 @@ class _BoardState extends State<Board> {
   @override
   Widget build(BuildContext context) {
     GameState state = GameState.getInstance();
+    double fontSize = state.size <= 9
+        ? 30.0
+        : state.size <= 16
+            ? 15.0
+            : 6.0;
 
     return Center(
       child: SizedBox(
@@ -37,6 +42,73 @@ class _BoardState extends State<Board> {
                 itemBuilder: (context, index) {
                   return Container(
                     color: const Color.fromARGB(255, 127, 132, 177),
+                  );
+                },
+              ),
+              GridView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                padding: EdgeInsets.zero,
+                itemCount: state.board.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: state.size,
+                  crossAxisSpacing: 2,
+                  mainAxisSpacing: 2,
+                ),
+                itemBuilder: (ctx, index) {
+                  return Container(
+                    color: Theme.of(context).highlightColor,
+                  );
+                },
+              ),
+              // Horizontal X-rule
+              GridView.builder(
+                padding: EdgeInsets.fromLTRB(
+                    (340 / (state.size)) / 2, 0, (340 / (state.size)) / 2, 0),
+                itemCount: state.board.length - state.size,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: state.size - 1,
+                    crossAxisSpacing: 2,
+                    mainAxisSpacing: 2),
+                itemBuilder: (context, index) {
+                  return Container(
+                    alignment: Alignment.center,
+                    child: state.xPositions.any((t) =>
+                            t.$1 ==
+                                ((index % (state.size - 1)) +
+                                    ((index ~/ (state.size - 1)) *
+                                        state.size)) &&
+                            t.$2 ==
+                                ((index % (state.size - 1)) +
+                                        (index ~/ (state.size - 1)) *
+                                            state.size) +
+                                    1)
+                        ? Text("X",
+                            style: TextStyle(
+                                fontSize: fontSize / 3 * 2,
+                                color: const Color.fromARGB(255, 19, 22, 54)))
+                        : const Text(""),
+                  );
+                },
+              ),
+              // Vertical X-rule
+              GridView.builder(
+                padding: EdgeInsets.fromLTRB(
+                    0, (340 / (state.size)) / 2, 0, (340 / (state.size)) / 2),
+                itemCount: state.board.length - state.size,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: state.size,
+                    crossAxisSpacing: 2,
+                    mainAxisSpacing: 2),
+                itemBuilder: (context, index) {
+                  return Container(
+                    alignment: Alignment.center,
+                    child: state.xPositions.any(
+                            (t) => t.$1 == index && t.$2 == t.$1 + state.size)
+                        ? Text("X",
+                            style: TextStyle(
+                                fontSize: fontSize / 3 * 2,
+                                color: const Color.fromARGB(255, 19, 22, 54)))
+                        : const Text(""),
                   );
                 },
               ),
--- lib/cell.dart
+++ lib/cell.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+import 'dart:ffi';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:sudoku/game_state.dart';
@@ -60,10 +62,6 @@ class _CellState extends State<Cell> {
   @override
   Widget build(BuildContext context) {
     var state = GameState.getInstance();
-
-    bool isX = state.xPositions
-        .any((t) => t.$1 == widget.index || t.$2 == widget.index);
-
     double fontSize = state.size <= 9
         ? 30.0
         : state.size <= 16
@@ -76,24 +74,27 @@ class _CellState extends State<Cell> {
       txtColor = Colors.amberAccent;
     }
 
-    return InkWell(
-      onTap: onClick,
-      child: Container(
-        color: isCurrentlyError
-            ? Colors.red
-            : (isX ? Colors.yellow : Theme.of(context).highlightColor),
-        alignment: Alignment.center,
-        child: widget.digit != null
-            ? Text(widget.digit!.toString(),
-                style: TextStyle(fontSize: fontSize, color: txtColor))
-            // 30 or 9x9, 15 for 16x16 , 6 for anything else (for now at least)
-            : Wrap(
-                spacing: 2.0,
-                children: state.drafts[widget.index]
-                    .map((n) => Text(n.toString()))
-                    .toList(),
-              ),
+    return Stack(children: [
+      Container(
+        color: isCurrentlyError ? Colors.red : Colors.transparent,
       ),
-    );
+      InkWell(
+        onTap: onClick,
+        child: Container(
+          color: Colors.transparent,
+          alignment: Alignment.center,
+          child: widget.digit != null
+              ? Text(widget.digit!.toString(),
+                  style: TextStyle(fontSize: fontSize, color: txtColor))
+              // 30 or 9x9, 15 for 16x16 , 6 for anything else (for now at least)
+              : Wrap(
+                  spacing: 2.0,
+                  children: state.drafts[widget.index]
+                      .map((n) => Text(n.toString()))
+                      .toList(),
+                ),
+        ),
+      ),
+    ]);
   }
 }
