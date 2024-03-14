708f743df1061c929fa40b295fe6162ba11aafc5
jegkanikkeheddemike <54175974+jegkanikkeheddemike@users.noreply.github.com>
2024-03-14 09:19:35 +0100

    upsi fix!!

--- solver/src/sudoku.rs
+++ solver/src/sudoku.rs
@@ -2,7 +2,7 @@ use std::{
     cmp::Ordering,
     fmt::{Display, Write},
     num::ParseIntError,
-    ops::Range,
+    ops::{Deref, Range},
     str::FromStr,
     sync::{atomic::AtomicUsize, mpsc, Arc, Mutex},
     time::Instant,
@@ -14,7 +14,7 @@ use lazy_static::lazy_static;
 use priority_queue::PriorityQueue;
 use rand::random;
 use regex_macro::regex;
-use smallvec::{SmallVec16, SmallVec8};
+use smallvec::{smallvec, SmallVec};
 use threadpool::ThreadPool;
 
 use crate::rules::{column_rule::ColumnRule, row_rule::RowRule, Rule};
@@ -24,8 +24,8 @@ pub type DynRule = Box<dyn Rule + Send>;
 #[derive(Debug)]
 pub struct Sudoku {
     pub size: usize,
-    pub cells: SmallVec16<Cell>,
-    pub rules: SmallVec8<DynRule>,
+    pub cells: Vec<Cell>,
+    pub rules: SmallVec<[DynRule; 8]>,
 }
 
 //Det her er ret fucked, men siden vi skal have den laveste entropy ud af vores priority queue skal den sammenligne omvendt
@@ -75,12 +75,7 @@ impl AllSolutionsContext {
         *GLOBAL_POOL.lock().unwrap() = Some(self.pool);
     }
 
-    fn add_branch(
-        &self,
-        old: &Sudoku,
-        cells: SmallVec16<Cell>,
-        new_queue: PriorityQueue<usize, Entropy>,
-    ) {
+    fn add_branch(&self, old: &Sudoku, cells: Vec<Cell>, new_queue: PriorityQueue<usize, Entropy>) {
         let mut new_sudoku = Sudoku {
             cells,
             ..old.clone()
@@ -130,21 +125,19 @@ impl Sudoku {
             rules.push(RowRule::new());
         }
 
-        let mut small_rules = SmallVec8::new();
-        small_rules.push_all_move(rules);
         Self {
             size,
             cells: (0..size * size)
                 .map(|_| Cell::new_with_range(1..(size as u16 + 1)))
                 .collect(),
-            rules: small_rules,
+            rules: rules.into(),
         }
     }
 
     pub fn set_cell(&mut self, n: u16, index: usize) -> Result<(), SudokuSolveError> {
         let mut ret_buffer = vec![];
         self.cells[index] = Cell::single(n);
-        for rule in self.rules.iter() {
+        for rule in &self.rules {
             for inner_index in rule
                 .updates(self.size, index, &mut ret_buffer)
                 .into_iter()
@@ -164,7 +157,7 @@ impl Sudoku {
         ret_buffer: &mut Vec<usize>,
     ) -> Result<(), SudokuSolveError> {
         self.cells[index] = Cell::single(n);
-        for rule in self.rules.iter() {
+        for rule in &self.rules {
             for inner_index in rule
                 .updates(self.size, index, ret_buffer)
                 .into_iter()
@@ -213,7 +206,7 @@ impl Sudoku {
             pri_queue
         };
 
-        let mut branch_stack: Vec<(SmallVec16<Cell>, PriorityQueue<usize, Entropy>)> = vec![];
+        let mut branch_stack: Vec<(Vec<Cell>, PriorityQueue<usize, Entropy>)> = vec![];
         let mut ret_buffer = vec![];
         let mut arena = Self::get_arena();
 
@@ -242,7 +235,7 @@ impl Sudoku {
                 _ => {
                     // Der er ikke flere naked singles, så der tjekkes for hidden singles
 
-                    for rule in self.rules.iter() {
+                    for rule in &self.rules {
                         if let Some((n, hidden_index)) = rule.hidden_singles(self) {
                             //Put nuværende cell tilbage i priority queue
                             pri_queue.push(index, entropy);
@@ -515,14 +508,14 @@ impl Display for Sudoku {
 
 #[derive(Debug, Clone)]
 pub struct Cell {
-    pub available: Vec<u16>,
+    pub available: smallvec::SmallVec<[u16; 16]>,
     pub locked_in: bool,
 }
 
 impl Cell {
     fn single(n: u16) -> Self {
         Self {
-            available: vec![n],
+            available: smallvec![n],
             locked_in: true,
         }
     }
@@ -541,7 +534,7 @@ impl Cell {
     }
     #[allow(unused)]
     pub fn is_single_eq(&self, n: u16) -> bool {
-        self.available == [n]
+        self.available.deref() == &[n]
     }
 }
 
