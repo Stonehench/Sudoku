edf4f289057729565eed169dc7232ef01cf1119c
Pinkymus <katinkaspangtoft@gmail.com>
2024-03-26 16:55:14 +0100

    gui

--- lib/zipper_rule_gui.dart
+++ lib/zipper_rule_gui.dart
@@ -11,15 +11,11 @@ class Zipper extends StatefulWidget {
 }
 
 class _ZipperState extends State<Zipper> {
+  Color lineColor = const Color.fromARGB(255, 152, 118, 175);
+
   @override
   Widget build(BuildContext context) {
     GameState state = GameState.getInstance();
-
-    double fontSize = state.size <= 9
-        ? 30.0
-        : state.size <= 16
-            ? 15.0
-            : 6.0;
 
     return Center(
       child: Stack(
@@ -38,15 +34,14 @@ class _ZipperState extends State<Zipper> {
                         zipper.$1 == index ||
                         zipper.$2.any((element) =>
                             element.$1 == index || element.$2 == index))
-                    ? SizedBox(
-                        height: 10,
+                    ? Container(
                         width: 10,
-                        child: Container(
-                          decoration: const BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Color.fromARGB(255, 129, 78, 163),
-                          ),
-                        ))
+                        height: 10,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: lineColor,
+                        ),
+                      )
                     : const Text(""),
               );
             },
@@ -119,23 +114,16 @@ class _ZipperState extends State<Zipper> {
                     if (j < zipper.$2.length - 1 &&
                             (left == index &&
                                     left + state.size == zipper.$2[j + 1].$1 ||
-                                left == index &&
-                                    left + state.size == zipper.$2[j + 1].$2 ||
-                                right == index &&
-                                    right + state.size == zipper.$2[j + 1].$1 ||
                                 right == index &&
                                     right + state.size ==
                                         zipper.$2[j + 1].$2) ||
                         j > 0 &&
                             (left == index &&
                                     left + state.size == zipper.$2[j - 1].$1 ||
-                                left == index &&
-                                    left + state.size == zipper.$2[j - 1].$2 ||
-                                right == index &&
-                                    right + state.size == zipper.$2[j - 1].$1 ||
                                 right == index &&
                                     right + state.size ==
                                         zipper.$2[j - 1].$2) ||
+                        // next to cente
                         zipper.$1 == index &&
                             zipper.$1 + state.size == zipper.$2.first.$1 ||
                         zipper.$1 == index &&
@@ -154,6 +142,62 @@ class _ZipperState extends State<Zipper> {
               );
             },
           ),
+
+          // diagonals
+          GridView.builder(
+            padding: EdgeInsets.fromLTRB(
+                (340 / (state.size)) / 2,
+                (340 / (state.size)) / 2,
+                (340 / (state.size)) / 2,
+                (340 / (state.size)) / 2),
+            itemCount: state.board.length - state.size,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: state.size - 1),
+            itemBuilder: (context, index) {
+              int i = ((index % (state.size - 1)) +
+                  ((index ~/ (state.size - 1)) * state.size));
+              return Container(
+                alignment: Alignment.center,
+                child: state.zipperPositions.any((zipper) {
+                  // if the center has a neighbour to the right
+                  for (int j = 0; j < zipper.$2.length; j++) {
+                    int left = zipper.$2[j].$1;
+                    int right = zipper.$2[j].$2;
+                    if (j < zipper.$2.length - 1 &&
+                            (left == i &&
+                                    left + state.size + 1 ==
+                                        zipper.$2[j + 1].$1 ||
+                                right == i &&
+                                    right + state.size + 1 ==
+                                        zipper.$2[j + 1].$2) ||
+                        j > 0 &&
+                            (left == i &&
+                                    left + state.size + 1 ==
+                                        zipper.$2[j - 1].$1 ||
+                                right == i &&
+                                    right + state.size + 1 ==
+                                        zipper.$2[j - 1].$2) ||
+                        // next to cente
+                        zipper.$1 == i &&
+                            zipper.$1 + state.size + 1 == zipper.$2.first.$1 ||
+                        zipper.$1 == i &&
+                            zipper.$1 + state.size + 1 == zipper.$2.first.$2 ||
+                        zipper.$2[j].$1 == zipper.$2.first.$1 &&
+                            zipper.$2[j].$1 == i &&
+                            zipper.$1 == i + state.size + 1 ||
+                        zipper.$2[j].$2 == zipper.$2.first.$2 &&
+                            zipper.$2[j].$2 == i &&
+                            zipper.$1 == i + state.size + 1) {
+                      return true;
+                    }
+                  }
+                  return false;
+                })
+                    ? getDiagonal(state, 0.785398163)
+                    : const Text(""),
+              );
+            },
+          ),
         ],
       ),
     );
@@ -164,7 +208,7 @@ class _ZipperState extends State<Zipper> {
         width: 340 / state.size,
         height: 10,
         child: Container(
-          color: const Color.fromARGB(255, 129, 78, 163),
+          color: lineColor,
         ));
   }
 
@@ -173,7 +217,17 @@ class _ZipperState extends State<Zipper> {
         height: 340 / state.size,
         width: 10,
         child: Container(
-          color: const Color.fromARGB(255, 129, 78, 163),
+          color: lineColor,
+        ));
+  }
+
+  Widget getDiagonal(GameState state, double angle) {
+    return Transform.rotate(
+        angle: angle,
+        child: Container(
+          width: 100,
+          height: 10,
+          color: lineColor,
         ));
   }
 }
