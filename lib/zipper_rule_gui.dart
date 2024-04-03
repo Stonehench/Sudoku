5941bd6a63979b1478fa6afaf656a8f256a1be6b
Pinkymus <katinkaspangtoft@gmail.com>
2024-04-03 22:23:58 +0200

    Working on pretty diagonals

--- lib/zipper_rule_gui.dart
+++ lib/zipper_rule_gui.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:sudoku/game_state.dart';
 
 class Zipper extends StatefulWidget {
@@ -215,6 +216,109 @@ class _ZipperState extends State<Zipper> {
             );
           },
         ),
+        GridView.builder(
+          padding: EdgeInsets.zero,
+          itemCount: state.board.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: state.size),
+          itemBuilder: (context, index) {
+            int i = index;
+            return Container(
+              alignment: Alignment.center,
+              child: state.zipperPositions.any((zipper) {
+                int s = state.size;
+                // if the center has a neighbour to the right
+                for (int j = 0; j < zipper.$2.length; j++) {
+                  int prevL = -1;
+                  int prevR = -1;
+                  int nextL = -1;
+                  int nextR = -1;
+                  if (j > 0) {
+                    prevL = zipper.$2[j - 1].$1;
+                    prevR = zipper.$2[j - 1].$2;
+                  }
+                  int left = zipper.$2[j].$1;
+                  int right = zipper.$2[j].$2;
+                  if (j < zipper.$2.length - 1) {
+                    nextL = zipper.$2[j + 1].$1;
+                    nextR = zipper.$2[j + 1].$2;
+                  }
+
+                  if ((prevL != -1 && left + s + 1 == prevL && i == left) ||
+                      (prevR != -1 && right + s + 1 == prevR && i == right) ||
+                      (nextL != -1 && left + s + 1 == nextL && i == left) ||
+                      (nextR != -1 && right + s + 1 == nextR && i == right) ||
+                      zipper.$1 == i &&
+                          (zipper.$2.first.$1 == zipper.$1 + s + 1 ||
+                              zipper.$2.first.$2 == zipper.$1 + s + 1) ||
+                      i == left &&
+                          left + s + 1 == zipper.$1 &&
+                          zipper.$2.first.$1 == left ||
+                      i == right &&
+                          right + s + 1 == zipper.$1 &&
+                          zipper.$2.first.$2 == right) {
+                    return true;
+                  }
+                }
+                return false;
+              })
+                  ? getSmallDiagonal(state, 0.785398163, Alignment.bottomRight)
+                  : const Text(""),
+            );
+          },
+        ),
+        /*
+        GridView.builder(
+          padding: EdgeInsets.zero,
+          itemCount: state.board.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: state.size),
+          itemBuilder: (context, index) {
+            int i = index;
+            return Container(
+              alignment: Alignment.center,
+              child: state.zipperPositions.any((zipper) {
+                int s = state.size;
+                // if the center has a neighbour to the right
+                for (int j = 0; j < zipper.$2.length; j++) {
+                  int prevL = -1;
+                  int prevR = -1;
+                  int nextL = -1;
+                  int nextR = -1;
+                  if (j > 0) {
+                    prevL = zipper.$2[j - 1].$1;
+                    prevR = zipper.$2[j - 1].$2;
+                  }
+                  int left = zipper.$2[j].$1;
+                  int right = zipper.$2[j].$2;
+                  if (j < zipper.$2.length - 1) {
+                    nextL = zipper.$2[j + 1].$1;
+                    nextR = zipper.$2[j + 1].$2;
+                  }
+
+                  if ((prevL != -1 && left - s - 1 == prevL && i == left) ||
+                      (prevR != -1 && right - s - 1 == prevR && i == right) ||
+                      (nextL != -1 && left - s - 1 == nextL && i == left) ||
+                      (nextR != -1 && right - s - 1 == nextR && i == right) ||
+                      zipper.$1 == i &&
+                          (zipper.$2.first.$1 == zipper.$1 - s - 1 ||
+                              zipper.$2.first.$2 == zipper.$1 - s - 1) ||
+                      i == left &&
+                          left - s - 1 == zipper.$1 &&
+                          zipper.$2.first.$1 == left ||
+                      i == right &&
+                          right - s - 1 == zipper.$1 &&
+                          zipper.$2.first.$2 == right) {
+                    return true;
+                  }
+                }
+                return false;
+              })
+                  ? getSmallDiagonal(state, 0.785398163, Alignment.topLeft)
+                  : const Text(""),
+            );
+          },
+        ), */
         // diagonals from top right to bottom left
         GridView.builder(
           padding: EdgeInsets.fromLTRB(
@@ -295,13 +399,38 @@ class _ZipperState extends State<Zipper> {
         ));
   }
 
+  Widget getSmallDiagonal(GameState state, double angle, Alignment alignment) {
+    return Align(
+      alignment: alignment,
+      child: SizedBox(
+          height: 200 / state.size,
+          width: 200 / state.size,
+          child: Align(
+            alignment: Alignment.center,
+            child: Transform.rotate(
+              angle: angle,
+              child: Container(
+                  height: 100 / state.size,
+                  width: 200 / state.size,
+                  color: lineColor),
+            ),
+          )),
+    );
+  }
+
   Widget getDiagonal(GameState state, double angle) {
-    return Transform.rotate(
-        angle: angle,
-        child: Container(
-          width: 1000,
-          height: 100 / state.size,
-          color: lineColor,
+    return SizedBox(
+        height: 340 / state.size,
+        width: 340 / state.size,
+        child: Align(
+          alignment: Alignment.center,
+          child: Transform.rotate(
+              angle: angle,
+              child: Container(
+                width: 340 / state.size,
+                height: 100 / state.size,
+                color: lineColor,
+              )),
         ));
   }
 }
