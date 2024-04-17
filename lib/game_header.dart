0bf412823581d4192206cf6ad902491aa3a36eb7
s224776 <s224776@student.dtu.dk>
2024-04-17 16:00:58 +0200

    pretty rules with explanation

--- lib/game_header.dart
+++ lib/game_header.dart
@@ -26,10 +26,124 @@ class GameHeader extends StatelessWidget {
                     showDialog(
                       context: context,
                       builder: (context) => AlertDialog(
-                        title: const Text("Rules"),
+                        title: const Align(
+                          alignment: Alignment.center,
+                          child: Text("Rules"),
+                        ),
                         content: IntrinsicHeight(
-                          child: Column(
-                            children: rules.map((e) => Text(e)).toList(),
+                          child: SizedBox(
+                            height: 300,
+                            child: RawScrollbar(
+                              child: SingleChildScrollView(
+                                child: Column(
+                                  ,
+                                  children: rules
+                                      .map(
+                                        (e) => e == "SquareRule"
+                                            ? const Column(
+                                                children: [
+                                                  Text(
+                                                    "Square Rule",
+                                                    style:
+                                                        TextStyle(fontSize: 20),
+                                                  ),
+                                                  Text(
+                                                      "Cells in the same square must be unique\n")
+                                                ],
+                                              )
+                                            : e == "KnightsMove"
+                                                ? const Column(
+                                                    children: [
+                                                      Text(
+                                                        "Knight Rule",
+                                                        style: TextStyle(
+                                                            fontSize: 20),
+                                                      ),
+                                                      Text(
+                                                          "Cells, a chess knights move away from eachother, can not be the same number\n")
+                                                    ],
+                                                  )
+                                                : e == "XRule"
+                                                    ? const Column(
+                                                        children: [
+                                                          Text(
+                                                            "X Rule",
+                                                            style: TextStyle(
+                                                                fontSize: 20),
+                                                          ),
+                                                          Text(
+                                                              "Cells with an X between them, must add to the sudoku's size plus one\n")
+                                                        ],
+                                                      )
+                                                    : e == "DiagonalRule"
+                                                        ? const Column(
+                                                            children: [
+                                                              Text(
+                                                                "Diagonal Rule",
+                                                                style: TextStyle(
+                                                                    fontSize:
+                                                                        20),
+                                                              ),
+                                                              Text(
+                                                                  "Cells must be unique from other cells on the same diagonal\n")
+                                                            ],
+                                                          )
+                                                        : e == "ParityRule"
+                                                            ? const Column(
+                                                                children: [
+                                                                  Text(
+                                                                    "Parity Rule",
+                                                                    style: TextStyle(
+                                                                        fontSize:
+                                                                            20),
+                                                                  ),
+                                                                  Text(
+                                                                      "Cells with a circle between them must have different parity\n")
+                                                                ],
+                                                              )
+                                                            : e == "ConsecutiveRule"
+                                                                ? const Column(
+                                                                    children: [
+                                                                      Text(
+                                                                        "Consecutive Rule",
+                                                                        style: TextStyle(
+                                                                            fontSize:
+                                                                                20),
+                                                                      ),
+                                                                      Text(
+                                                                          "Cells with a dot between them must be one apart\n")
+                                                                    ],
+                                                                  )
+                                                                : e == "ZipperRule"
+                                                                    ? const Column(
+                                                                        children: [
+                                                                          Text(
+                                                                            "Zipper Rule",
+                                                                            style:
+                                                                                TextStyle(fontSize: 20),
+                                                                          ),
+                                                                          Text(
+                                                                              "Zippers appears as pink lines with a circle in the center."
+                                                                              "The two cells on the line with equal distance from the center, must add to the center\n")
+                                                                        ],
+                                                                      )
+                                                                    : e == "ThermometerRule"
+                                                                        ? const Column(
+                                                                            children: [
+                                                                              Text(
+                                                                                "Thermometer Rule",
+                                                                                style: TextStyle(fontSize: 20),
+                                                                              ),
+                                                                              Text("Thermometers appears as blue lines with a circle in one end."
+                                                                                  "Cells starting at the dot must be in rising order\n")
+                                                                            ],
+                                                                          )
+                                                                        : Text(e),
+                                      )
+                                      .toList(),
+                                ),
+                              ),
+                            ),
                           ),
                         ),
                       ),
@@ -43,7 +157,7 @@ class GameHeader extends StatelessWidget {
               ),
             ],
           ),
-          const Text("Standard Sudoku", style: TextStyle(fontSize: 24)),
+          const Text("Standard Sudoku", style: TextStyle(fontSize: 30)),
         ],
       ),
     );
