15d9f7f97a2404a076647966941d64503132b703
Kapkapper <kasperfj@hotmail.dk>
2024-06-20 14:21:33 +0200

    Update menu.dart

--- lib/menu.dart
+++ lib/menu.dart
@@ -26,6 +26,7 @@ class _MenuState extends State<Menu> {
   int size = 9;
   final inputTextController = TextEditingController();
 
+  // Function for updating text and size base textfield further down
   void onTextChange(String newText) {
     int? newSize = int.tryParse(newText);
     if (newSize != null) {
@@ -54,6 +55,7 @@ class _MenuState extends State<Menu> {
     }
   }
 
+  // Name and state of rules, for checkboxes further down
   final List<(String, String, bool)> rules = [
     ("Square rule", "SquareRule", true),
     ("Knights move", "KnightsMove", false),
@@ -67,9 +69,11 @@ class _MenuState extends State<Menu> {
 
   bool initialized = false;
 
+  // Function for adding rule checkboxes
   List<Widget> ruleWidgets() {
     List<Widget> list = [];
 
+    // Extraction of real name for displaying
     for (var (name, realname, def) in rules) {
       if (!initialized) {
         initialized = true;
@@ -77,7 +81,7 @@ class _MenuState extends State<Menu> {
           gameModes.add(realname);
         }
       }
-
+      // Adds the rule names and checkboxes to
       list.add(
         SizedBox(
           width: 140,
@@ -109,8 +113,10 @@ class _MenuState extends State<Menu> {
     return list;
   }
 
+  // hide size box by default
   bool showSizeBox = false;
 
+  // initial selection of size
   List<bool> sizeValueStates = <bool>[
     false,
     true,
@@ -118,6 +124,7 @@ class _MenuState extends State<Menu> {
     false,
   ];
 
+  // Values for size selection
   List<int> sizeValues = [
     4,
     9,
@@ -125,6 +132,7 @@ class _MenuState extends State<Menu> {
     0,
   ];
 
+  // Get size from text
   getSizeFromText() {
     if (inputTextController.text == "") {
       return 4;
@@ -133,11 +141,13 @@ class _MenuState extends State<Menu> {
     }
   }
 
+  // Selection buttons for size selection
   Widget sudokuSizeWidget() {
     return ToggleButtons(
       direction: Axis.horizontal,
       onPressed: (int index) {
         setState(() {
+          // Set size and control visibility of sizebox
           if (index == 3) {
             showSizeBox = true;
             for (int i = 0; i < sizeValueStates.length; i++) {
@@ -157,6 +167,7 @@ class _MenuState extends State<Menu> {
           }
         });
       },
+      // Visual configurations
       borderRadius: const BorderRadius.all(Radius.circular(8)),
       borderColor: Colors.transparent,
       constraints: const BoxConstraints(
@@ -170,8 +181,10 @@ class _MenuState extends State<Menu> {
     );
   }
 
+  // Initial difficulty
   String gameDifficulty = "Medium";
 
+  // Initial selection of difficulty
   List<bool> difficulitiesValues = <bool>[
     false,
     true,
@@ -179,6 +192,7 @@ class _MenuState extends State<Menu> {
     false,
   ];
 
+  // Difficulty options
   List<String> difficulitiesNames = [
     "Easy",
     "Medium",
@@ -186,6 +200,7 @@ class _MenuState extends State<Menu> {
     "Expert",
   ];
 
+  // Selection buttons for difficulty selection
   Widget difficulitiesWidgets() {
     return ToggleButtons(
       direction: Axis.horizontal,
@@ -216,6 +231,7 @@ class _MenuState extends State<Menu> {
   String? dailyDate;
   bool notLoggedIn = false;
 
+  // 
   @override
   Widget build(BuildContext context) {
     AccountState accState = AccountState.instance();
